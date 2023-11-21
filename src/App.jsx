d64aad5c92fa96c38cba4bfef14afb646b58f025
ShubhdeepDasCardioTrack <shubhdeep.das@cardiotrack.io>
2023-11-21 15:37:46 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <p className='text-3xl font-bold'>Yo this is shanto's started kit ! </p>
+        <Button>Start creating ?</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
